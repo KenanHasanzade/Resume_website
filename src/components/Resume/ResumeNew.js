062d58a55b12dd1c45f9480a640cb473196b326b
KenanHasanzade <kenanhsn034@gmail.com>
2023-09-28 13:09:26 +0200

    update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -45,7 +45,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download CV 
           </Button>
         </Row>
       </Container>
